e52ad3aa084116a604f7afbf2d6db0406b9ca5f5
Frederik Spang <frederik@progras.dk>
2018-02-12 13:38:27 +0100

    Adds testcase

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,9 @@
 public class Runner {
   public static void main(String[] args) {
-    System.out.printf("YES");
+    System.out.printf((new Runner()).printed());
+  }
+
+  public java.lang.String printed(){
+    return "yes";
   }
 }
