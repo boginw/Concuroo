d3223d2dd8ae5faa12f40c17001e530c120644d8
boginw <bogi.wennerstrom@gmail.com>
2018-04-24 12:37:23 +0200

    P4-179: Added structure for testing ASTVisitor

---
+++ src/test/java/concuroo/ASTVisitorTest.java
@@ -0,0 +1,86 @@
+package concuroo;
+
+import static org.junit.Assert.*;
+
+import ConcurooParser.ConcurooLexer;
+import ConcurooParser.ConcurooParser;
+import ConcurooParser.ConcurooParser.*;
+import concuroo.nodes.Node;
+import concuroo.nodes.statement.CompoundStatement;
+import concuroo.nodes.statement.IterationStatement;
+import concuroo.nodes.statement.jumpStatement.ReturnStatement;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.Test;
+
+public class ASTVisitorTest {
+
+  @Test
+  public void visitEmptyCompoundStatement() {
+    ConcurooParser parser = parse("{}");
+    CompoundStatementContext ctx = parser.compoundStatement();
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof CompoundStatement);
+    assertEquals(0, ((CompoundStatement) n).size());
+  }
+
+  @Test
+  public void visitNonEmptyCompoundStatement() {
+    ConcurooParser parser = parse("{return;}");
+    CompoundStatementContext ctx = parser.compoundStatement();
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof CompoundStatement);
+    assertEquals(1, ((CompoundStatement) n).size());
+    assertTrue(((CompoundStatement) n).getStatement(0) instanceof ReturnStatement);
+  }
+
+  @Test
+  public void visitIterationStatement() {
+    ConcurooParser parser = parse("while(true) return");
+    IterationStatementContext ctx = parser.iterationStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof IterationStatement);
+    // assertTrue(((IterationStatement) n).getCondition() instanceof BooleanLiteral);
+    assertTrue(((IterationStatement) n).getConsequence() instanceof ReturnStatement);
+  }
+
+  @Test
+  public void visitIfStatement() {
+  }
+
+  @Test
+  public void visitJumpStatement() {
+  }
+
+  @Test
+  public void visitAssignmentExpression() {
+  }
+
+  @Test
+  public void visitAdditiveExpression() {
+  }
+
+  @Test
+  public void visitMultiplicativeExpression() {
+  }
+
+  @Test
+  public void visitLogicalAndExpression() {
+  }
+
+  @Test
+  public void visitLogicalOrExpression() {
+  }
+
+  @Test
+  public void visitEqualityExpression() {
+  }
+
+  public ConcurooParser parse(String input){
+    ConcurooLexer lex = new ConcurooLexer(CharStreams.fromString(input));
+    ConcurooParser parser = new ConcurooParser(new CommonTokenStream(lex));
+    parser.setBuildParseTree(true);
+    return parser;
+  }
+}
