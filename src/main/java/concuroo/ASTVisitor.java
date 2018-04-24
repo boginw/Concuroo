99da1bf470f01c775c3b3b1962b9a71a6f7f0e9e
scarress <mikkel.xzx@gmail.com>
2018-04-24 11:05:50 +0200

    Added Literals for char, bool, double, int, and strings. They have been implemented in the ASTVisitor. Does not take Identifiers into account

--- src/main/java/concuroo/ASTVisitor.java
+++ src/main/java/concuroo/ASTVisitor.java
@@ -2,19 +2,15 @@ package concuroo;
 
 import ConcurooParser.ConcurooBaseVisitor;
 import ConcurooParser.ConcurooParser;
-
 import concuroo.nodes.Node;
 import concuroo.nodes.expression.Expression;
-import concuroo.nodes.expression.LiteralExpression;
-import concuroo.nodes.expression.PrimaryExpression;
 import concuroo.nodes.expression.binaryExpression.arithmeticBinaryExpression.AdditiveExpression;
 import concuroo.nodes.expression.binaryExpression.arithmeticBinaryExpression.MultiplicativeExpression;
 import concuroo.nodes.expression.binaryExpression.logicalBinaryExpression.LogicalAndExpression;
 import concuroo.nodes.expression.binaryExpression.logicalBinaryExpression.LogicalEqualityExpression;
 import concuroo.nodes.expression.binaryExpression.logicalBinaryExpression.LogicalOrExpression;
 import concuroo.nodes.expression.literalExpression.*;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.tree.ParseTree;
+
 
 public class ASTVisitor extends ConcurooBaseVisitor<Node> {
 
@@ -24,7 +20,6 @@ public class ASTVisitor extends ConcurooBaseVisitor<Node> {
     if (ctx.children.size() == 3) {
       return null;
     }
-
 
     if (!ctx.StringLiteral().isEmpty()){
       return new StringLiteral(ctx.StringLiteral().toString());
@@ -38,38 +33,22 @@ public class ASTVisitor extends ConcurooBaseVisitor<Node> {
           return new CharLiteral(token.charAt(1));
         }
       }
+
       else if (Character.isDigit(token.charAt(0))) {
         if (token.contains(".")) {
           return new FloatLiteral(Double.valueOf(token));
         }
         return new IntLiteral(Integer.valueOf(token));
       }
+
       else if (token.equals("false")){
         return new BoolLiteral(false);
       }
+
       else return new BoolLiteral(true);
-
-
+      }
     }
-
-
-//    else if ( == ConcurooParser.T__38) {
-//    }
-
-    //ParseTree node = visitTerminal(ctx.ConstantLiteral());
-
-//  if (visit(ctx.getChild(0)).getLiteral().indexOf(0) == '"'){
-//    StringLiteral lit = new StringLiteral(ctx.getChild(0).getText());
-//    return lit;
-//  }
-//  else if (Character.isDigit(visit(ctx.getChild(0)).toString().indexOf(0))){
-//    IntLiteral lit = new IntLiteral(Integer.parseInt(ctx.getChild(0).getText()));
-//    return lit;
-//  }
-
-
-    }
-    return super.visitPrimaryExpression(ctx);
+    return null;
   }
 
   @Override
