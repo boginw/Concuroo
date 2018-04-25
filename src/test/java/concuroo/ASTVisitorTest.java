e9d50292d76fa933a7e40ad13c108d46ea7cd8a1
TOBIASPALLUDAN\Tobias <tpallu16@student.aau.dk>
2018-04-25 10:02:36 +0200

    Doing some testing, but blocked by missing literals

--- src/test/java/concuroo/ASTVisitorTest.java
+++ src/test/java/concuroo/ASTVisitorTest.java
@@ -6,9 +6,16 @@ import ConcurooParser.ConcurooLexer;
 import ConcurooParser.ConcurooParser;
 import ConcurooParser.ConcurooParser.*;
 import concuroo.nodes.Node;
+import concuroo.nodes.expression.Expression;
+import concuroo.nodes.expression.binaryExpression.logicalBinaryExpression.LogicalEqualityExpression;
 import concuroo.nodes.statement.CompoundStatement;
 import concuroo.nodes.statement.IterationStatement;
+import concuroo.nodes.statement.JumpStatement;
+import concuroo.nodes.statement.SelectionStatement;
+import concuroo.nodes.statement.jumpStatement.BreakStatement;
+import concuroo.nodes.statement.jumpStatement.ContinueStatement;
 import concuroo.nodes.statement.jumpStatement.ReturnStatement;
+import concuroo.nodes.statement.selectionStatement.IfStatement;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.Test;
@@ -46,11 +53,66 @@ public class ASTVisitorTest {
   }
 
   @Test
-  public void visitIfStatement() {
+  public void visitIfStatementNoElse() {
+    ConcurooParser parser = parse("if(1 == 1) return;");
+    SelectionStatementContext ctx = parser.selectionStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof IfStatement);
+    assertTrue(((IfStatement) n).getCondition() instanceof Expression);
+    assertTrue(((IfStatement) n).getConsequence() instanceof ReturnStatement);
+    assertNull(((IfStatement) n).getAlternative());
   }
 
   @Test
-  public void visitJumpStatement() {
+  public void visitIfStatementWithElse() {
+    ConcurooParser parser = parse("if(1 == 1) return else return;");
+    SelectionStatementContext ctx = parser.selectionStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof IfStatement);
+    assertTrue(((IfStatement) n).getCondition() instanceof LogicalEqualityExpression);
+    assertTrue(((IfStatement) n).getConsequence() instanceof ReturnStatement);
+    assertTrue(((IfStatement) n).getAlternative() instanceof ReturnStatement);
+  }
+
+  @Test
+  public void visitJumpStatementBreak() {
+    ConcurooParser parser = parse("break;");
+    JumpStatementContext ctx = parser.jumpStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof BreakStatement);
+  }
+
+  @Test
+  public void visitJumpStatementContinue() {
+    ConcurooParser parser = parse("continue;");
+    JumpStatementContext ctx = parser.jumpStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof ContinueStatement);
+  }
+
+  @Test
+  public void visitJumpStatementReturn() {
+    ConcurooParser parser = parse("return;");
+    JumpStatementContext ctx = parser.jumpStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof ReturnStatement);
+    assertNull(((ReturnStatement) n).getReturnValue());
+  }
+
+  @Test
+  public void visitJumpStatementReturnWithValue() {
+    ConcurooParser parser = parse("return 1;");
+    JumpStatementContext ctx = parser.jumpStatement();
+
+    Node n = new ASTVisitor().visit(ctx);
+    assertTrue(n instanceof ReturnStatement);
+    assertTrue(((ReturnStatement) n).getReturnValue() instanceof );
+
   }
 
   @Test
