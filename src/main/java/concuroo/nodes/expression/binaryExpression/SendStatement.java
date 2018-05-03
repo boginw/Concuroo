75c9a27032424e428e3ede25c8a9753b8debfdee
TOBIASPALLUDAN\Tobias <tpallu16@student.aau.dk>
2018-05-03 18:27:17 +0200

    P4-189 Added visitor and AST for sendstatement

---
+++ src/main/java/concuroo/nodes/expression/binaryExpression/SendStatement.java
@@ -0,0 +1,9 @@
+package concuroo.nodes.expression.binaryExpression;
+
+public class SendStatement extends BinaryExpression {
+
+  @Override
+  public String getOperator() {
+    return "<-";
+  }
+}
